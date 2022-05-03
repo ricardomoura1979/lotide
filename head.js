d851234b6819cca96800a9802d4ab8dfc36d3840
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-03 00:43:52 +0000

    done

---
+++ head.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("✅✅✅" + "Assertion Passed" + "[" + actual + "] === [" + expected + "]");
+  } else {
+    console.log("🛑🛑🛑" + "Assertion Failed" + "[" + actual + "] !== [" + expected + "]");
+
+  }
+  
+};
+const head = function(array)  {
+  return array[0];
+  
+
+}
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+/*
+_.head([1, 2, 3]);
+// => 1
+ 
+_.head([]);
+// => undefined
+*/
