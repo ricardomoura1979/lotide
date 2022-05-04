0405fd32fb1b32877a25772fc9fea9c82375f7ea
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-04 01:38:11 +0000

    done

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+
+
+const assertEqual = function(actual, expected) {
+
+  
+  if (actual === expected) {
+    console.log("✅✅✅" + "Assertion Passed" + "[" + actual + "] === [" + expected + "]");
+  } else {
+    console.log("🛑🛑🛑" + "Assertion Failed" + "[" + actual + "] !== [" + expected + "]");
+
+  }
+};
+const eqArrays = function(actual, expected) {
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]){
+        return false
+      } 
+    }
+  } else {
+    return false;
+
+  }
+  return true;
+}
+
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
+
