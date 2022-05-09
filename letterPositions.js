34455ba801caaad169dbcd4b6869516d48d82e46
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-09 19:23:33 +0000

    done

---
+++ letterPositions.js
@@ -0,0 +1,48 @@
+
+
+const assertEqual = function(actual, expected) {
+
+  
+  if (actual === expected) {
+    console.log("✅✅✅" + "Assertion Passed" + "[" + actual + "] === [" + expected + "]");
+  } else {
+    console.log("🛑🛑🛑" + "Assertion Failed" + "[" + actual + "] !== [" + expected + "]");
+
+  }
+};
+const eqArrays = function(actual, expected) {
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]){
+        return false
+      } 
+    }
+  } else {
+    return false;
+
+  }
+  return true;
+}
+
+
+
+
+const letterPositions = (sentence) => {
+  const letters = {};
+  for (const key of sentence) {
+    if (key !== ' '){ 
+      if (letters[key]) {
+      letters[key] += 1;
+    } else {
+      letters[key] = 1;
+    }
+    }
+    
+  } 
+
+  return letters;
+};
+
+console.log(assertEqual(eqArrays(letterPositions("hello").e, [1])));
+/* console.log(letterPositions('hello'));
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS */
