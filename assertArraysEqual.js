60ae86722aa7b8bcc9f91811b184e2df3aeb781a
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-09 19:22:06 +0000

    done

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,17 +13,30 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(actual, expected) {
   if (actual.length === expected.length) {
     for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]){
-        console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]))); 
-      } 
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
     }
   } else {
     return false;
 
   }
   return true;
-}
+};
 
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
+
+
+/* Challenge
+Instruction
+Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+
+The message will be similar to that of assertEqual. In fact, you could refer to the code for assertEqual to help you implement this. Instead of doing a simple === check though, this assertion function will make use of your eqArrays function for array comparison.
+
+Warning
+Functions that do not return values and instead print out messages to the console are more difficult to test using assertions. This is because we cannot simply compare their return value to an expected value.
+
+For this reason, we will not be writing assertions (test code) for assertArraysEqual. That said, you can and should call the function below its declaration and visually ensure that the message printed to the console is accurate.
+ */
