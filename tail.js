34c91c7fdae498bcbd46d6613401f2edbd17978e
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-09 19:23:19 +0000

    done

--- tail.js
+++ tail.js
@@ -12,6 +12,6 @@ const tail = function(array) {
   return array;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+const words = ["Yo Yo", "Lighthouse", "Labs", "Test"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words.length, 4); // original array should still have 3 elements!
