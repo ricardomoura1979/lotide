4d15da8ab731d7121cd2955ad2680b04928a960b
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-09 22:21:55 +0000

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,6 @@
 
 
-const assertEqual = function(actual, expected) {
 
-  
-  if (actual === expected) {
-    console.log("✅✅✅" + "Assertion Passed" + "[" + actual + "] === [" + expected + "]");
-  } else {
-    console.log("🛑🛑🛑" + "Assertion Failed" + "[" + actual + "] !== [" + expected + "]");
-
-  }
-};
 const eqArrays = function(actual, expected) {
   if (actual.length === expected.length) {
     for (let i = 0; i < actual.length; i++) {
@@ -25,11 +16,9 @@ const eqArrays = function(actual, expected) {
 }
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
+
+
+module.exports = eqArrays;
 
