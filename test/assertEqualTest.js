a4b8f3236a830b1af3970575b7cd802437e4ee35
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-09 19:41:21 +0000

    refactored tail inside

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -6,3 +6,7 @@ const assertEqual = require('../assertEqual');
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+// tail.js below:
+
+assertEqual("Yo Yo", "Lighthouse", "Labs", "Test")
