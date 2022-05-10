12a86f0e4785e78d86cfbe57924398e24e15e89c
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-10 00:53:46 +0000

    refactored

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,27 @@
-const middle = require('../middle');
-const assertEqual = require('../assertArraysEqual');
+//const middle = require('../middle');
+//const assertEqual = require('../assertArraysEqual'); */
+
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#head", () => {
+  
+  it("returns empty []  for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns empty []  for [1, 2]", () => {
+     assert.deepEqual(middle([1, 2]), []);
+  }); 
+  it("returns empty [2, 3]  for [2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns empty []  for [3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
 
 
 
 
-
-
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
