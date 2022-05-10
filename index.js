677f679742735d13bb586155e673c58b2ef4dafe
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-10 17:49:52 +0000

    export using shorthand

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 
-module.exports = {
+/* module.exports = {
   head:   head,
   tail:   tail,
   middle: middle
 };
+ */
+module.exports = {head, tail, middle};
