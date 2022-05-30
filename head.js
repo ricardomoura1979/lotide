9ea9e04220ec9824e48326269b359d740a05ba04
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-30 20:16:41 +0000

    head with assertEqual has been completed/

--- head.js
+++ head.js
@@ -1,4 +1,12 @@
-
+const assertEqual = function(actual, expected) {
+  const actual1 = actual;
+  const expected1 = expected;
+  if (actual === expected) {
+    console.log(`"✅" + "Assertion Passed:  ${actual1}  === ${expected1}`);
+  } else {
+    console.log(`"🛑" + "Assertion Failed:  ${actual1}  !== ${expected1}`);
+  }
+};
 
 const head = function(array)  {
   return array[0];
@@ -6,5 +14,8 @@ const head = function(array)  {
 
 };
 
-module.exports = head
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//module.exports = head
+//assertEqual was inserted to remake bootcamp tasks. After this, we have to insert module.exports above and delete this line and the assertEqual function.
 
