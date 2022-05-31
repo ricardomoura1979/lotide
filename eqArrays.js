7a03df664b88600804acac39e3a8317108f783b4
Ricardo Moura <ricardomoura1979@gmail.com>
2022-05-31 20:50:40 +0000

    nothing has been changed

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,16 @@
+// REINSERTED assertEqual function. After, it will be necessary to remove again. When exporting this file as module.
+const assertEqual = function(actual, expected) {
+  const actual1 = actual;
+  const expected1 = expected;
+  if (actual === expected) {
+    console.log(`"✅Assertion Passed:  ${actual1}  === ${expected1}`);
+  } else {
+    console.log(`"🛑Assertion Failed:  ${actual1}  !== ${expected1}`);
+
+  }
+   
+
+}; 
 
 
 
@@ -16,9 +29,13 @@ const eqArrays = function(actual, expected) {
 }
 
 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
 
 
 
-
-module.exports = eqArrays;
+//module.exports = eqArrays; // this module must to be reinserted when we learn how to do it.
 
