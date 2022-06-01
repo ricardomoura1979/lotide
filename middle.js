00f27a52d17c971b945b86c82b1abf0d2740dffa
Ricardo Moura <ricardomoura1979@gmail.com>
2022-06-01 17:56:01 +0000

    updated with assertEqual and eqArrays

--- middle.js
+++ middle.js
@@ -1,26 +1,33 @@
-// const assertEqual = function(actual, expected) {
 
-  
-//   if (actual === expected) {
-//     console.log("✅✅✅" + "Assertion Passed" + "[" + actual + "] === [" + expected + "]");
-//   } else {
-//     console.log("🛑🛑🛑" + "Assertion Failed" + "[" + actual + "] !== [" + expected + "]");
+const assertEqual = function(actual, expected) {
+  const actual1 = actual;
+  const expected1 = expected;
+  if (actual === expected) {
+    console.log(`"✅Assertion Passed:  ${actual1}  === ${expected1}`);
+  } else {
+    console.log(`"🛑Assertion Failed:  ${actual1}  !== ${expected1}`);
 
-//   }
-// };
-// const eqArrays = function(actual, expected) {
-//   if (actual.length === expected.length) {
-//     for (let i = 0; i < actual.length; i++) {
-//       if (actual[i] !== expected[i]){
-//         return false
-//       }
-//     }
-//   } else {
-//     return false;
+  }
+   
 
-//   }
-//   return true;
-// }
+}; 
+
+
+
+const eqArrays = function(actual, expected) {
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]){
+        return false
+      } 
+    }
+  } else {
+    return false;
+
+  }
+  return true;
+}
+
 const middle = function(array) {
   let middleArray = [];
   if (array.length <= 2) {
@@ -44,4 +51,4 @@ console.log(middle([1, 2])); // => []
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4] 
 
-module.exports = middle
+//module.exports = middle // we are gonna export in the future
