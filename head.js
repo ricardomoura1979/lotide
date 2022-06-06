7921f8ca41a2ffef0f881f8befe25c5f3c4acb03
Ricardo Moura <ricardomoura1979@gmail.com>
2022-06-06 18:14:11 +0000

    added module to import assertEqual.js to test the file

--- head.js
+++ head.js
@@ -1,13 +1,5 @@
-const assertEqual = function(actual, expected) {
-  const actual1 = actual;
-  const expected1 = expected;
-  if (actual === expected) {
-    console.log(`"✅" + "Assertion Passed:  ${actual1}  === ${expected1}`);
-  } else {
-    console.log(`"🛑" + "Assertion Failed:  ${actual1}  !== ${expected1}`);
-  }
-};
 
+const assertEqual = require('./assertEqual');
 const head = function(array)  {
   return array[0];
   
@@ -16,6 +8,6 @@ const head = function(array)  {
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-//module.exports = head
-//assertEqual was inserted to remake bootcamp tasks. After this, we have to insert module.exports above and delete this line and the assertEqual function.
+module.exports = head
+
 
